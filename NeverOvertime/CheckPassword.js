f143428bb56a10e206b14dcf44219ba839423c9b
BrandonTimm <46364864+BrandonTimm@users.noreply.github.com>
2022-02-08 10:23:02 -0500

    Added CheckPassword.js

---
+++ NeverOvertime/CheckPassword.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+function checkPass(name,password){
+    if (!fs.existsSync('./profiles/' + name + '.json')){
+        return 1 //No username found
+    }
+    const jsonString = fs.readFileSync('./profiles/' + name + '.json');
+    const profile = JSON.parse(jsonString);
+    console.log(profile);
+    if (profile.password == password){
+        return 0; //Successful
+    }
+    else{
+        return 2; //Password Incorrect
+    }
+}
