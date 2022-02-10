d945ff05b6187afbd9bd247b79517611009db9d4
Lucas <random@gmail.com>
2022-02-09 22:19:44 -0500

    finished the login screen

--- NeverOvertime/LogIn.js
+++ NeverOvertime/LogIn.js
@@ -7,6 +7,7 @@ import {
   StyleSheet,
   Section,
   Text,
+  TextInput,
   useColorScheme,
   View,
   Button,
@@ -16,14 +17,42 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+
+
 function LogIn ({ navigation }) {
+
+  const [text1, ChangeText1] = React.useState(null);
+  const [text2, ChangeText2] = React.useState(null);
+
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Log In</Text>
-
+            <TextInput
+              style={box.input}
+              onChangeText={ChangeText1}
+              value={text1}
+              placeholder="Username"
+            />
+            <TextInput
+              style={box.input}
+              onChangeText={ChangeText2}
+              value={text2}
+              placeholder="Password"
+            />
             <Button title="Log In" onPress={() => navigation.navigate('Home')} />
       </View>
     );
 }
+
+const box = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 10,
+    borderWidth: 1,
+    width: 150,
+    padding: 7,
+  },
+});
+
 
 export default LogIn;
