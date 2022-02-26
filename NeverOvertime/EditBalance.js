c81b61d66ba4d31b9098004adbdefe1e5d387cf5
BrandonTimm <46364864+BrandonTimm@users.noreply.github.com>
2022-02-26 14:58:15 -0500

    Updated EditBalance.js, added addBal and subBal

--- NeverOvertime/EditBalance.js
+++ NeverOvertime/EditBalance.js
@@ -1,6 +1,40 @@
+const fs = require('fs');
+function addBal(name,amount){
+    var currBal = checkBal(name);
+    var newBal = {
+        "balance": currBal+amount
+    };
+    var jsonString = JSON.stringify(newBal);
+
+    fs.writeFile('./balances/' + name + '.json', jsonString, err => {
+        if (err) {
+            console.log('Error writing file', err);
+        }
+    });
+    return 0; //Success
+}
+
+function subBal(name,amount){
+    var currBal = checkBal(name);
+    if (currBal < amount){
+        return 1; //Not enough money in balance
+    }
+    var newBal = {
+        "balance": currBal-amount
+    };
+    var jsonString = JSON.stringify(newBal);
+
+    fs.writeFile('./balances/' + name + '.json', jsonString, err => {
+        if (err) {
+            console.log('Error writing file', err);
+        }
+    });
+    return 0; //Success
+}
+
 function checkBal(name){
     var bal = require('./balances/' + name + '.json');
     return bal.balance;
 }
 
-module.exports = {checkBal}
+module.exports = {addBal, subBal, checkBal}
