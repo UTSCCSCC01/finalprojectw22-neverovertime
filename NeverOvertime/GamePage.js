9f92f53dc80c20eca76143647bf40a7b56653407
tonyz961 <shengjie.zou@mail.utoronto.ca>
2022-02-28 20:34:19 -0500

    Update GamePage.js

--- NeverOvertime/GamePage.js
+++ NeverOvertime/GamePage.js
@@ -1,7 +1,3 @@
-
-import Deck from "react-poker";
-import "react-poker/styles.css"
-
 import React from 'react';
 import type {Node} from 'react';
 import {
@@ -19,16 +15,27 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
+//import drawCard from "./DrawCard.js";
+
 
 function GamePage ({ navigation, route }) {
+    function drawCard(){
+        Alert.alert(
+          "Drawing Cards is yet to be implemented", "Click OK",
+          [
+            {text: "OK"}
+          ]
+        )
+    }
+
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-
-
+      <Text>GamePage</Text>
+      <Button title="Draw Cards" onPress={() => drawCard()
+          } />
 
     </View>
   );
-
+}
 
 export default GamePage;
