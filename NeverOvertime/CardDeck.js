2d07651313cbff3bc007b0f04bca823600b1b1a6
kksong11 <honglei.song@mail.utoronto.ca>
2022-03-02 18:47:12 -0500

    NEV-41 add functions and classes for making a deck of cards

---
+++ NeverOvertime/CardDeck.js
@@ -0,0 +1,44 @@
+const SUITS = ["club", "heart", "spade", "diamond"]
+const RANKS = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
+class Card{
+    constructor(rank, suit){
+        this.rank = rank
+        this.suit = suit
+    }
+}
+
+export class Deck {
+    constructor (cards = newDeck()) {
+        this.cards = cards
+    }
+
+    get cardNumber(){
+        return this.cards.length
+    }
+
+    shuffle(){
+        for (let i = 0; i < this.cardNumber - 1; i++){
+            const newIndex = Math.floor(Math.random() * (i+1))
+            const oldRank = this.cards[newIndex]
+            this.cards[newIndex] = this.cards[i]
+            this.cards[i] = oldRank
+
+        }
+    }
+
+    dealCard(){
+        return this.cards.pop();
+
+    }
+}
+
+export function newDeck() {
+    return SUITS.flatMap(suit => {
+        return RANKS.map(rank => {
+            return new Card(rank, suit)
+        })
+    })
+}
+
+module.exports = {newDeck}
