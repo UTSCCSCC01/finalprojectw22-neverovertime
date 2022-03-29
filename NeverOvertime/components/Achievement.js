2b7803b2bedd4485394c95708b82ca7a374b147e
tonyz961 <shengjie.zou@mail.utoronto.ca>
2022-03-29 00:12:34 -0400

    Update Achievement.js

--- NeverOvertime/components/Achievement.js
+++ NeverOvertime/components/Achievement.js
@@ -3,29 +3,36 @@ import { Button, View, Text, StyleSheet, FlatList, TouchableOpacity, Image } fro
 
 /* Defines all the achievements and view for the achievement page. */
 function Achievement({}) {
+  const [refresh, setRefresh] = useState(false);
   const [achievement, setAchievement] = useState([
-    {text: 'Win 1 round of blackjack', prize: 'Reward $10', key : 1}, /* no key property and any other; change in Flatlist*/
-    {text: 'Win 10 round of blackjack', prize: 'Reward $100', key : 2},
-    {text: 'Win 100 round of blackjack', prize: 'Reward $1,000', key : 3},
-    {text: 'Win 1000 round of blackjack', prize: 'Reward $10,000', key : 4},
-    {text: 'Win 10000 round of blackjack', prize: 'Reward $100,000', key : 5},
-    {text: 'Win 5 rounds in a row', prize: 'Reward $1,000', key : 6},
-    {text: 'Win 10 rounds in a row	', prize: 'Reward $2,000', key : 7},
-    {text: 'Win 20 rounds in a row', prize: 'Reward $4,000', key : 8},
-    {text: 'Win 30 rounds in a row', prize: 'Reward $8,000', key : 9},
-    {text: 'Win 50 rounds in a row', prize: 'Reward $16,000', key : 10},
-    {text: 'Get a blackjack(21)', prize: 'Reward $500', key : 11},
-    {text: 'Get 2 blackjack(21) in a row', prize: 'Reward $2,000', key : 12},
-    {text: 'Get 3 blackjack(21) in a row', prize: 'Reward $8,000', key : 13},
-    {text: 'Get 4 blackjack(21) in a row', prize: 'Reward $24,000', key : 14},
-    {text: 'Get 5 blackjack(21) in a row', prize: 'Reward $100,000', key : 15},
-    {text: 'First time Bankrupt', prize: 'Reward $500', key : 16},
-    {text: '5th time Bankrupt', prize: 'Reward $2,000', key : 17},
-    {text: '10th time Bankrupt', prize: 'Reward $4,000', key : 18},
-    {text: '15th time Bankrupt', prize: 'Reward $8,000', key : 19},
-    {text: '20th time Bankrupt', prize: 'Reward $16,000', key : 20},
+    {text: 'Win 1 round of blackjack', prize: 'Reward $10', key : 1, disabled: false}, /* no key property and any other; change in Flatlist*/
+    {text: 'Win 10 round of blackjack', prize: 'Reward $100', key : 2, disabled: false},
+    {text: 'Win 100 round of blackjack', prize: 'Reward $1,000', key : 3, disabled: false},
+    {text: 'Win 1000 round of blackjack', prize: 'Reward $10,000', key : 4, disabled: false},
+    {text: 'Win 10000 round of blackjack', prize: 'Reward $100,000', key : 5, disabled: false},
+    {text: 'Win 5 rounds in a row', prize: 'Reward $1,000', key : 6, disabled: false},
+    {text: 'Win 10 rounds in a row	', prize: 'Reward $2,000', key : 7, disabled: false},
+    {text: 'Win 20 rounds in a row', prize: 'Reward $4,000', key : 8, disabled: false},
+    {text: 'Win 30 rounds in a row', prize: 'Reward $8,000', key : 9, disabled: false},
+    {text: 'Win 50 rounds in a row', prize: 'Reward $16,000', key : 10, disabled: false},
+    {text: 'Get a blackjack(21)', prize: 'Reward $500', key : 11, disabled: false},
+    {text: 'Get 2 blackjack(21) in a row', prize: 'Reward $2,000', key : 12, disabled: false},
+    {text: 'Get 3 blackjack(21) in a row', prize: 'Reward $8,000', key : 13, disabled: false},
+    {text: 'Get 4 blackjack(21) in a row', prize: 'Reward $24,000', key : 14, disabled: false},
+    {text: 'Get 5 blackjack(21) in a row', prize: 'Reward $100,000', key : 15, disabled: false},
+    {text: 'First time Bankrupt', prize: 'Reward $500', key : 16, disabled: false},
+    {text: '5th time Bankrupt', prize: 'Reward $2,000', key : 17, disabled: false},
+    {text: '10th time Bankrupt', prize: 'Reward $4,000', key : 18, disabled: false},
+    {text: '15th time Bankrupt', prize: 'Reward $8,000', key : 19, disabled: false},
+    {text: '20th time Bankrupt', prize: 'Reward $16,000', key : 20, disabled: false},
   ])
-
+    const completeAchievement = (key) => {
+        let ach = achievement.find((a) => {return a.key == key});
+        ach.disabled = true;
+        alert(key);
+//        setAchievement(achievement);
+        setRefresh(!refresh);
+    }
 
     return (
       <View style={styles.container}>
@@ -39,9 +46,10 @@ function Achievement({}) {
               /*keyExtractor={(item) => item.text} if key property is change to another id*/
               /*In TouchableOpacity, missing onPress property because achievement system has not set up yet */
               data={achievement}
+              extraData={refresh}
               renderItem={({ item }) => (
-                <TouchableOpacity >
-                  <View style={styles.item}>
+                <TouchableOpacity onPress={() => completeAchievement(item.key)} disabled = {item.disabled}>
+                  <View style={styles.item} >
                     <Text >{item.prize}</Text>
                     <Text >{item.text}</Text>
                   </View>
